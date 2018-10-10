89fb6eb70303fe2cf4070683d76d9063744f5f2b
Dominik.Kotecki <dominik.kotecki@rossmann.com.pl>
2018-10-10 11:38:41 +0200

    electron init

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+var path = require("path");
+var webpack = require("webpack");
+
+function resolve(filePath) {
+  return path.join(__dirname, filePath)
+}
+
+var babelOptions = {
+  presets: [
+      ["@babel/preset-env", {
+          "targets": {
+              "browsers": ["last 2 versions"]
+          },
+          "modules": false
+      }]
+  ]
+};
+
+var isProduction = process.argv.indexOf("-p") >= 0;
+console.log("Bundling for " + (isProduction ? "production" : "development") + "...");
+
+var basicConfig = {
+  devtool: "source-map",
+  node: {
+    __dirname: false,
+    __filename: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.fs(x|proj)?$/,
+        use: {
+          loader: "fable-loader",
+          options: {
+            babel: babelOptions,
+            define: isProduction ? [] : ["DEBUG"]
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: babelOptions
+        },
+      }
+    ]
+  }
+};
+
+var mainConfig = Object.assign({
+  target: "electron-main",
+  entry: resolve("src/Main/Main.fsproj"),
+  output: {
+    path: resolve("app"),
+    filename: "main.js"
+  }
+}, basicConfig);
+
+var rendererConfig = Object.assign({
+  target: "electron-renderer",
+  entry: resolve("src/Renderer/Renderer.fsproj"),
+  output: {
+    path: resolve("app"),
+    filename: "renderer.js"
+  }
+}, basicConfig);
+
+module.exports = [mainConfig, rendererConfig]
