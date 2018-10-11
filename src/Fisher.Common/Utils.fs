88f633eacc1fd389eb81d41b285c8c0a79672985
dominikus1993 <dominikus1910@gmail.com>
2018-10-11 23:10:38 +0200

    small changes

--- src/Fisher.Common/Utils.fs
+++ src/Fisher.Common/Utils.fs
@@ -1,6 +1,12 @@
 namespace Fisher.Common
 
 open System
+open MathNet.Numerics.Statistics
+open MathNet.Numerics.Statistics
+open MathNet.Numerics.Statistics
 
 module Fisher =
-    let a = 2
+    let F(matrixA: double seq)(matrixB: double seq)  =
+        (Statistics.Mean(matrixA) - Statistics.Mean(matrixB)) / (Statistics.StandardDeviation(matrixA) / Statistics.StandardDeviation(matrixB))
+
+    let FMD(matrixA: double seq)(matrixB: double seq)  =
